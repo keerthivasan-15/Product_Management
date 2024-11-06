3077811f2c9f0131adeb91e45c82a80fbcb834af
keerthivasan-15 <keerthivasan0016@gmail.com>
2024-11-06 20:14:30 +0530

    "readme.md file added"

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 app.use("/api/products",productRoutes);
